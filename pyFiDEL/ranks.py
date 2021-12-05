38fb315ddc7421a5b8f3e816821c6dd0bb4c15e4
Sungcheol Kim <kimsung@us.ibm.com>
2021-12-05 08:02:01 -0500

    add Tutorial jupyter notebook and update ranks, simulator

--- pyFiDEL/ranks.py
+++ pyFiDEL/ranks.py
@@ -8,9 +8,12 @@ __author__ = 'Sung-Cheol Kim'
 __version__ = '1.0.0'
 
 import numpy as np
+import pandas as pd
+from scipy import special
+import scipy
 
 
-def auc_rank(scores: list, y: list):
+def auc_rank(scores: list, y: list) -> float:
     ''' calculate AUC using rank formula '''
 
     if len(scores) != len(y):
@@ -28,5 +31,153 @@ def auc_rank(scores: list, y: list):
     if ans < .5:
         print('... class label might be wrong!')
         ans = 1 - .5
+
+    return ans
+
+
+def build_metric(scores: list, y: list, method='min') -> (pd.DataFrame, dict):
+    ''' calculate all metrics using rank formula '''
+
+    # prepare data frame
+    df = pd.DataFrame({'score': scores, 'y': y})
+    df.sort_values(by=['score'], ignore_index=True, inplace=True)
+    df['rank'] = range(len(y) + 1)[1:]
+
+    t_rank = df['y'] == 'Y'
+    f_rank = df['y'] == 'N'
+    N = len(y)
+    N1 = np.sum(t_rank)
+    N2 = N - N1
+    rho = float(N1 / N)
+    auc0 = auc_rank(scores, y)
+
+    df['tpr'] = np.cumsum(t_rank) / N1
+    df['fpr'] = np.cumsum(f_rank) / N2
+    df['bac'] = .5 * (df['tpr'] + 1. - df['fpr'])
+    df['prec'] = np.cumsum(t_rank) / df['rank']
+
+    info = {
+        'auc_rank': auc0,
+        'auc_bac': 2. * df['bac'].mean() - .5,
+        'auprc': .5 * N1 / N * (1. + N / (N1 * N1) * np.sum(df['prec'][1:N - 2] * df['prec'][2: N - 1])),
+        'rho': rho,
+    }
+    if method == 'min':
+        info.update(get_fermi_min(auc0, rho))
+    else:
+        info.update(get_fermi_root(auc0, rho))
+
+    return df, info
+
+
+def get_fermi_min(auc: float, rho: float, N: int = 1, resol: float = 0.0001, method: str = 'beta') -> dict:
+    ''' calculate beta and mu (or l1 and l2) from AUC and rho '''
+
+    # check auc range
+    if auc < .5:
+        auc = 1. - auc
+
+    lambdas = get_lambda(auc, N=1000, rho=rho)
+    initials = [lambdas['l2'] * 1000, -lambdas['l1'] / (1000 * lambdas['l2'])]
+
+    # find beta, mu
+    # temp = scipy.optimize.fmin(_cost, initials, args=(auc, rho, resol), maxiter=8000, ftol=1E-6)
+    res = scipy.optimize.minimize(_cost, initials, args=(auc, rho, resol))
+    temp = res.x
+
+    r_star = 1. / temp[0] * np.log((1. - rho) / rho) + temp[1]
+
+    if method == 'beta':
+        return {
+            'beta': temp[0] / N,
+            'mu': temp[1] * N,
+            'r_star': r_star * N,
+        }
+    else:
+        return {
+            'l1': -temp[0] * temp[1],
+            'l2': temp[1] / N,
+            'r_star': r_star * N,
+        }
+
+
+def _cost(bm: list, auc: float, rho: float, resol: float) -> float:
+    ''' cost function to minimize '''
+
+    r_prime = np.linspace(0, 1., num=int(1. / resol), endpoint=False)
+    sum1 = np.sum(resol / (1. + np.exp(bm[0] * (r_prime - bm[1]))))
+    sum2 = np.sum(resol * r_prime / (1. + np.exp(bm[0] * (r_prime - bm[1]))))
+
+    diff1 = rho - sum1
+    diff2 = .5 * rho - rho * (1. - rho) * (auc - .5) - sum2
+
+    return diff1 * diff1 + diff2 * diff2
+
+
+def get_fermi_root(auc: float, rho: float, N: int = 1) -> dict:
+    ''' calculate beta and mu from AUC and rho '''
+
+    lambdas = get_lambda(auc, N=1000, rho=rho)
+    beta0 = -lambdas['l1'] / (1000. * lambdas['l2'])
+
+    beta = scipy.optimize.brentq(_froot, 0.05, beta0 * 5., args=(auc, rho))
+    mu = .5 - np.log(np.sinh(beta * (1. - rho) * .5) / np.sinh(beta * rho * .5)) / beta
+    r_star = 1. / beta * np.log((1. - rho) / rho) + mu
+
+    return {
+        'beta': beta / N,
+        'mu': mu * N,
+        'r_star': r_star * N,
+    }
+
+
+def _froot(beta: float, auc: float, rho: float) -> float:
+    ''' cost function for auc and rho '''
+
+    mu = .5 - np.log(np.sinh(beta * (1. - rho) * .5) / np.sinh(beta * rho * .5)) / beta
+    part1 = (scipy.special.spence(1. + np.exp(beta * (mu - 1.))) - scipy.special.spence(1. + np.exp(beta * mu)) - beta * np.log(np.exp(beta * (mu - 1.)) + 1.)) / (beta * beta)
+    part2 = .5 * rho - rho * (1. - rho) * (auc - .5)
+    print(auc, rho, mu, '-', part1, part2)
+
+    return part1 - part2
+
+
+def get_lambda(auc: float, N: int = 1000, rho: float = .5) -> dict:
+    ''' calculate lambda1, lambda2 from auc, rho '''
+
+    N = float(N)
+    l1_low = np.log(1. / rho - 1.) - 12. * N * (auc - .5) / (N * N - 1.) * ((N + 1 + N * rho) * .5 - N * rho * auc)
+    l2_low = 12. * N * (auc - .5) / (N * N - 1.)
+
+    temp = np.sqrt(rho * (1. - rho) * (1. - 2. * (auc - .5)))
+    l1_high = -2. * rho / (np.sqrt(3) * temp)
+    l2_high = 2. / (np.sqrt(3) * N * temp)
+
+    alpha = 2. * (auc - .5)
+    l1 = l1_high * alpha + l2_low * (1. - alpha)
+    l2 = l2_high * alpha + l2_low * (1. - alpha)
+    r_star = 1. / l2 * np.log((1. - rho) / rho) - l1 / l2
+
+    return {
+        'l1low': l1_low,
+        'l2low': l2_low,
+        'l1high': l1_high,
+        'l2high': l2_high,
+        'l1': l1,
+        'l2': l2,
+        'r_star': r_star,
+    }
+
+
+def build_correspond_table(auclist: list, rholist: list, resol: float = 0.001) -> pd.DataFrame:
+    ''' calculate correspondence table between (auc, rho) and (beta, mu) '''
+
+    ans = pd.DataFrame()
+
+    for auc in auclist:
+        for rho in rholist:
+            row = {'auc': auc, 'rho': rho}
+            row.update(get_fermi_min(auc, rho, resol=resol))
+            ans = ans.append(row, ignore_index=True)
 
     return ans
--- pyFiDEL/simulator.py
+++ pyFiDEL/simulator.py
@@ -9,8 +9,10 @@ __version__ = '1.0.0'
 
 import numpy as np
 from scipy import special
+import pandas as pd
+import seaborn as sns
 
-from ranks import auc_rank
+from .ranks import auc_rank
 
 
 class SimClassifier(object):
@@ -21,12 +23,15 @@ class SimClassifier(object):
     def __init__(self, N: int = 1000, rho: float = .5):
         self.N = N
         self.rho = rho
+        self.N1 = int(self.N * self.rho)
+        self.N2 = N - self.N1
 
-    def create_gaussian_scores(self, auc0: float = .9, y: list = [], tol: float = 0.0001, max_iter: int = 2000):
+        y = ['Y'] * self.N1 + ['N'] * self.N2
+        self.y = np.array(y)
+        self.score = None
+
+    def create_gaussian_scores(self, auc0: float = .9, tol: float = 0.0001, max_iter: int = 2000):
         ''' create gaussian scores to match AUC '''
-
-        N1 = self.N * self.rho
-        N2 = self.N - N1
 
         count = 0
         mu = 2. * special.erfinv(2. * auc0 - 1)
@@ -35,17 +40,35 @@ class SimClassifier(object):
         # create score distribution by iterating creation of normal distribution
         simulated_auc = .5
         while abs(simulated_auc - auc0) > tol and count < max_iter:
-            score1 = np.random.normal(0, 1, N1)
-            score2 = np.random.normal(mu, 1, N2)
+            score1 = np.random.normal(0, 1, self.N1)
+            score2 = np.random.normal(mu, 1, self.N2)
 
             score = np.zeros(self.N)
-            score[y == 'Y'] = score1
-            score[y == 'N'] = score2
+            score[self.y == 'Y'] = score1
+            score[self.y == 'N'] = score2
 
-            simulated_auc = auc_rank(score, y)
+            simulated_auc = auc_rank(score, self.y)
 
             count += 1
 
         print(f'Final AUC: {simulated_auc} (iter: {count}) mu2: {mu}')
+        self.score = score
 
         return score
+
+    def plot_o(self):
+        ''' build data for  '''
+
+    def plot_score(self):
+        ''' plot histogram of scores '''
+
+        if self.score is None:
+            print('create scores first.')
+            return
+
+        df = pd.DataFrame()
+        df['score'] = self.score
+        df['y'] = self.y
+
+        sns.histplot(data=df, x='score', hue='y', hue_order=['Y', 'N'])
+
