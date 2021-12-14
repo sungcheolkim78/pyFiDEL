9e6ef36a6bcd14da010749bdab96b508d28b2e36
Sungcheol Kim <kimsung@us.ibm.com>
2021-12-13 21:35:05 -0500

    add ci.py

---
+++ pyFiDEL/ci.py
@@ -0,0 +1,91 @@
+'''
+ci.py - confidence interval calculation
+
+Soli Deo Gloria
+'''
+
+__author__ = 'Sung-Cheol Kim'
+__version__ = '1.0.0'
+
+import numpy as np
+
+from .utils import fermi_b
+from .ranks import get_fermi_root
+
+
+def Pxy_int(beta: float, mu: float, rho: float, resol: float = 1E-6) -> float:
+    ''' calculate Pxy with integral formula '''
+
+    print(f'... Pxy integral calculation with resolution = {resol}')
+
+    p_table = fermi_b(np.linspace(0, 1, num=int(1. / resol)), b=beta, m=mu, normalized=False)
+
+    A = p_table
+    B = np.linspace(len(p_table), 1, num=len(p_table)) - np.cumsum(p_table[::-1])[::-1]
+
+    ans = np.sum(A * B) - np.sum(A * (1. - A))
+    ans = ans * resol * resol / (rho * (1. - rho))
+
+    return ans
+
+
+def Pxxy_int(beta: float, mu: float, rho: float, resol: float = 1E-6) -> float:
+    ''' calculate Pxxy iwth integral formula '''
+
+    print(f'... Pxxy integral calculation with resolution = {resol}')
+
+    p_table = fermi_b(np.linspace(0, 1, num=int(1. / resol)), b=beta, m=mu, normalized=False)
+
+    A = 2. * p_table * np.cumsum(p_table) - p_table * p_table
+    B = np.linspace(len(p_table), 1, num=len(p_table)) - np.cumsum(p_table[::-1])[::-1]
+
+    ans = np.sum(A * B) - np.sum(p_table * p_table * (1. - p_table)) - np.sum(p_table * p_table) - 2. * np.sum(p_table * (1. - p_table))
+    ans = ans * resol * resol * resol / (rho * rho * (1. - rho))
+
+    return ans
+
+
+def Pxyy_int(beta: float, mu: float, rho: float, resol: float = 1E-6) -> float:
+    ''' calculate Pxyy iwth integral formula '''
+
+    print(f'... Pxyy integral calculation with resolution = {resol}')
+
+    p_table = fermi_b(np.linspace(0, 1, num=int(1. / resol)), b=beta, m=mu, normalized=False)
+
+    A = p_table
+    B = np.cumsum((1. - p_table)[::-1])[::-1] ** 2
+
+    ans = np.sum(A * B) - np.sum(p_table * (1. - p_table) * (1. - p_table)) - np.sum((1. - p_table) ** 2) - 2. * np.sum(p_table * (1. - p_table))
+    ans = ans * resol * resol * resol / (rho * (1. - rho) * (1. - rho))
+
+    return ans
+
+
+def var_auc_fermi(auc: float, rho: float, N: int, resol: float = 1E-6, debug: bool = False):
+    ''' calculate variance of AUC from Fermi-Dirac distribution '''
+
+    bm = get_fermi_root(auc, rho)
+    N1 = int(N * rho)
+    N2 = N - N1
+
+    Pxy_value = Pxy_int(bm['beta'], bm['mu'], rho, resol=resol)
+    Pxxy_value = Pxxy_int(bm['beta'], bm['mu'], rho, resol=resol)
+    Pxyy_value = Pxyy_int(bm['beta'], bm['mu'], rho, resol=resol)
+
+    var_auc = (Pxy_value * (1. - Pxy_value)
+               + (N1 - 1.) * (Pxxy_value - Pxy_value * Pxy_value)
+               + (N2 - 1.) * (Pxyy_value - Pxy_value * Pxy_value)) / (N1 * N2)
+    std_auc = np.sqrt(var_auc)
+
+    return {
+        'var_auc': var_auc,
+        'Pxy': Pxy_value,
+        'Pxxy': Pxxy_value,
+        'Pxyy': Pxyy_value,
+        'beta': bm['beta'],
+        'mu': bm['mu'],
+        'auc0': auc,
+        'rho': rho,
+        'auc sigma': std_auc,
+        '95% ci': [auc - 1.96 * std_auc, auc + 1.96 * std_auc],
+    }
